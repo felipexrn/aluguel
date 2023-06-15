f2873f24caa36638625a65f02c316dbdad6f68a2
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-06-15 03:07:35 +0000

    e la vamos nos

--- mysite/aluguel/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name = models.CharField(max_length=30, blank=False)
-    description = models.CharField(max_length=200)
-    #theme = models.ForeignKey('Theme', on_delete=models.CASCADE, related_name='itens')
-
-    def __str__(self):
-        return self.name
